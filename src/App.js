ceaa1bba423eea2132c1316e688a54716c449560
Burcu Altınok <burcualtnok@gmail.com>
2017-12-01 17:21:36 +0000

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,50 +1,127 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import bilgi from './bilgi.json';
+//import bilgi from './this.state.bilgi.json';
 import Experience from "./Experience";
 import Interests from "./Interets";
 import Projects from "./Projects";
 import Skill from "./Skill";
 import Language from "./Language";
 import Education from "./Education";
+import firebase from 'firebase';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bilgi:{
+            "Experiences":[
+                {
+                    "baslik":"",
+                    "lokasyon":"",
+                    "tarih":"",
+                    "aciklama":""
+                }
+            ],
+            "Projects":[
+                {
+                    "projectName":"",
+                    "url":"",
+                    "aciklama":""
+                }
+            ],
+            "Skills":[
+                {
+                    "skill":"C",
+                    "yuzde":"60%"
+                },
+                {
+                    "skill":"Java",
+                    "yuzde":"60%"
+                },
+                {
+                    "skill":"Python",
+                    "yuzde":"50%"
+                },
+                {
+                    "skill":"JavaScript",
+                    "yuzde":"40%"
+                },
+                {
+                    "skill":"Django",
+                    "yuzde":"30%"
+                },
+                {
+                    "skill":"MVC",
+                    "yuzde":"20%"
+                }
+            ],
+
+           "Education" : [
+              {
+                "baslik": "Science-Mathematics",
+                "icerik": "Tire Kutsan Anatolian High School",
+                "tarih": "2009 - 2013"
+              }
+            ],
+
+            "Languages": [
+              {
+                "dil": "Turkish",
+                "derece": "Native"
+              }
+            ],
+
+            "Interests": [
+              {"hobi": "Travelling"}
+            ]
+      }
+    };
+  }
+componentDidMount() {
+    var vRef = firebase.database().ref();
+
+    vRef.on('value', snapshot => {
+      this.setState({bilgi: snapshot.val()});
+    });
+  }
+
+
   render() {
     return (
       <div className="wrapper">
           <div className="sidebar-wrapper">
               <div className="profile-container">
-                  <img className="profile" src={bilgi.image} alt=""
+                  <img className="profile" src={this.state.bilgi.image} alt=""
                   width="100px" height="100px"/>
-                  <h1 className="name"> {bilgi.name} </h1>
+                  <h1 className="name"> {this.state.bilgi.name} </h1>
                   <h3 className="tagline">Developer</h3>
               </div>
 
               <div className="contact-container container-block">
                   <ul className="list-unstyled contact-list">
-                      <li className="email"><i className="fa fa-envelope"></i><a href={"mailto:" +  bilgi.email}>{bilgi.email}</a></li>
-                      <li className="phone"><i className="fa fa-phone"></i><a href={bilgi.phone}>{bilgi.phone}</a></li>
-                      <li className="website"><i className="fa fa-globe"></i><a href={bilgi.website} target="_blank">{bilgi.website}</a></li>
-                      <li className="twitter"><i className="fa fa-twitter"></i><a href={bilgi.twitterlink} target="_blank">{bilgi.twitter}</a></li>
+                      <li className="email"><i className="fa fa-envelope"></i><a href={"mailto:" +  this.state.bilgi.email}>{this.state.bilgi.email}</a></li>
+                      <li className="phone"><i className="fa fa-phone"></i><a href={this.state.bilgi.phone}>{this.state.bilgi.phone}</a></li>
+                      <li className="website"><i className="fa fa-globe"></i><a href={this.state.bilgi.website} target="_blank">{this.state.bilgi.website}</a></li>
+                      <li className="twitter"><i className="fa fa-twitter"></i><a href={this.state.bilgi.twitterlink} target="_blank">{this.state.bilgi.twitter}</a></li>
                   </ul>
               </div>
              <div className="education-container container-block">
                   <h2 className="container-block-title">Education</h2>
-                  {bilgi.Education.map((item, key)=><Education item={item} key={key}/>)}
+                  {this.state.bilgi.Education.map((item, key)=><Education item={item} key={key}/>)}
               </div>
 
 
               <div className="languages-container container-block">
                   <h2 className="container-block-title">Languages</h2>
                   <ul className="list-unstyled interests-list">
-                      {bilgi.Languages.map((item, key)=> <Language item={item} key={key}/>)}
+                      {this.state.bilgi.Languages.map((item, key)=> <Language item={item} key={key}/>)}
                   </ul>
               </div>
 
               <div className="interests-container container-block">
                   <h2 className="container-block-title">Interests</h2>
                   <ul className="list-unstyled interests-list">
-                      {bilgi.Interests.map((item,key) => <Interests item={item} key={key}/>)}
+                      {this.state.bilgi.Interests.map((item,key) => <Interests item={item} key={key}/>)}
                   </ul>
               </div>
 
@@ -56,14 +133,14 @@ class App extends Component {
               <section className="section summary-section">
                   <h2 className="section-title"><i className="fa fa-user"></i>Career Profile</h2>
                   <div className="summary">
-                      <p>{bilgi.Career}</p>
+                      <p>{this.state.bilgi.Career}</p>
                   </div>
               </section>
 
               <section className="section experiences-section">
                   <h2 className="section-title"><i className="fa fa-briefcase"></i>Experiences</h2>
 
-                {bilgi.Experiences.map((item,key) => <Experience item={item} key={key} />)}
+                {this.state.bilgi.Experiences.map((item,key) => <Experience item={item} key={key} />)}
 
               </section>
 
@@ -71,15 +148,15 @@ class App extends Component {
               <section className="section projects-section">
                   <h2 className="section-title"><i className="fa fa-archive"></i>Projects</h2>
                   <div className="intro">
-                      <p>{bilgi.ProjectDescription}</p>
+                      <p>{this.state.bilgi.ProjectDescription}</p>
                   </div>
-                  {bilgi.Projects.map((item,key)=> <Projects item={item} key={key}/>)}
+                  {this.state.bilgi.Projects.map((item,key)=> <Projects item={item} key={key}/>)}
               </section>
 
               <section className="skills-section section">
                   <h2 className="section-title"><i className="fa fa-rocket"></i>Skills &amp; Proficiency</h2>
                   <div className="skillset">
-                      {bilgi.Skills.map((item,key)=> <Skill item={item} key={key}/>)}
+                      {this.state.bilgi.Skills.map((item,key)=> <Skill item={item} key={key}/>)}
                   </div>
               </section>
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDJxrNdlWYVDXujcIYGPB6sH7V5LcJnqTY",
+    authDomain: "mycvreact.firebaseapp.com",
+    databaseURL: "https://mycvreact.firebaseio.com",
+    projectId: "mycvreact",
+    storageBucket: "mycvreact.appspot.com",
+    messagingSenderId: "332464741756"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
